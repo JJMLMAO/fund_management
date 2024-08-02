41f9e8e3446d5b2c9bce35c24aaa4ae78b9f24f7
JJMLMAO <lje72399.lje@gmail.com>
2024-08-03 03:51:34 +0800

    add drf

---
+++ fund_mgmt_system/funds/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Fund
+
+class FundSerializers(serializers.ModelSerializer):
+    class Meta:
+        model =Fund
+        fields = '__all__'
--- fund_mgmt_system/funds/views.py
+++ fund_mgmt_system/funds/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Fund
+from .serializers import FundSerializers
 
 # Create your views here.
+
+class FundViewSet(viewsets.ModelViewSet):
+    queryset = Fund.objects.all()
+    serializer_class = FundSerializers
